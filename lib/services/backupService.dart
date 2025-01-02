a87b83d0041c8b50bda340b1d6ddf732b0988b75
ITCoderz <135115047+ITCoderz@users.noreply.github.com>
2025-01-03 01:26:51 +0500

    removed shared prefrences
    Removed Shared prefrences

--- lib/services/backupService.dart
+++ lib/services/backupService.dart
@@ -1,36 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 
 class BackupService {
-  static Future<String?> backupSharedPreferences() async {
-    try {
-      // Get all SharedPreferences data
-      final prefs = await SharedPreferences.getInstance();
-      final allPrefs = prefs.getKeys().fold<Map<String, dynamic>>({}, (map, key) {
-        map[key] = prefs.get(key);
-        return map;
-      });
-
-      // Convert to JSON
-      final jsonString = jsonEncode(allPrefs);
-
-      // Get the directory to save the file
-      final directory = await getApplicationDocumentsDirectory();
-      final backupFile = File('${directory.path}/shared_prefs_backup.json');
-
-      // Save JSON to a file
-      await backupFile.writeAsString(jsonString);
-
-      return backupFile.path; // Return the backup file path
-    } catch (e) {
-      print('Error during backup: $e');
-      return null;
-    }
-  }
-  static Future<String?> backupDatabaseToSQL(Database db) async {
+ static Future<String?> backupDatabaseToSQL(Database db) async {
     try {
       // Get all tables
       final tables = await db.rawQuery(
